4b951e0191258eb1965464a7823e6266d62000b7
draver-git <xato@coldemail.info>
2014-03-25 14:52:54 +0400

    Commit 1
    Description for commit 1.

---
+++ Source/Struct1/struct1.cs
@@ -0,0 +1,32 @@
+﻿using System;/*< Подключается область имен "System"*/
+struct SimpleStruct  /*< Описание структуры "SimpleStruct"*/
+{
+    private int xval; //!< Обявление переменной xval
+    public int X //!< Обявление переменной X 
+    {
+        get 
+        {
+            return xval;
+        } //!< Функция взятия переменной
+        set 
+        {
+            if (value < 100)
+                xval = value;
+        } //!< Функция установки переменной
+    }
+    public void DisplayX() //! обьявление метода
+    {
+        Console.WriteLine("The stored value is: {0}", xval);
+    }
+}
+
+class TestClass /*<Создается класс "TestClass"*/
+{
+    public static void Main()/*< Вызывается функция "Main"*/
+    {
+        SimpleStruct ss = new SimpleStruct();//! Создается элемент структуры  "SimpleStruct"
+        ss.X = 5; //! Переменной X элемента структуры ss приравнивается значение
+        ss.DisplayX(); //! Выводит значение элемента структуры
+    }
+}
+
---
+++ Source/Struct2/struct2.cs
@@ -0,0 +1,38 @@
+﻿using System;/*< Подключается область имен "System"*/
+
+class TheClass //! Обьявляется класс TheClass
+{
+    public int x; //! Обьявляется переменная
+}
+
+struct TheStruct //! Обьявляется структура TheStruct
+{
+    public int x; //! Обьявляется переменная
+}
+
+class TestClass //! Обьявляется класс TestClass
+{
+    public static void structtaker(TheStruct s) //! Обьявляется функция класса TestClass
+    {
+        s.x = 5;//! Элементу структуры s присваивается значение
+    }
+    public static void classtaker(TheClass c) //! Обьявляется функция класса TestClass
+    {
+        c.x = 5;//! Элементу класса c присваивается значение
+    }
+    public static void Main() //! Обьявляется функция класса TestClass
+    {
+        TheStruct a = new TheStruct(); //! Создается экземпляр структуры TheStruct
+        TheClass b = new TheClass();//! Создается экземпляр класса TheClass
+        a.x = 1;//! Элементу структуры а присваивается значение
+        b.x = 1;//! Элементу класса b присваивается значение
+        structtaker(a);//! Вызов функции класса TestClass
+        classtaker(b);//! Вызов функции класса TestClass
+        Console.WriteLine("a.x = {0}", a.x);
+        //! Вызов консоли и вывод результата
+        Console.WriteLine("b.x = {0}", b.x);
+        //! Вызов консоли и вывод результата
+    }
+    
+}
+
